0a131c16544bed06ac1d90fe7131b4f6c137a923
TephdyTech <justsepf24@gmail.com>
2026-09-21 11:51:34 +0800

    fix for css not rendering

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: [
+  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
+],
   darkMode: "class",
   theme: {
     extend: {
